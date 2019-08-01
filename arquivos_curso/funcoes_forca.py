8208e53635e446c97dbd21b79fc65ceff7b6f46a
thaisandre <thaisandre.sc@gmail.com>
2019-08-01 04:45:26 -0300

    arquivos do curso

---
+++ arquivos_curso/funcoes_forca.py
@@ -0,0 +1,82 @@
+def imprime_mensagem_perdedor(palavra_secreta):
+    print('Puxa, você foi enforcado!')
+    print('A palavra era {}'.format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \        ")
+    print("  /                 \       ")
+    print("//                   \/\    ")
+    print("\|   XXXX     XXXX   | /    ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/      ")
+    print("   |\     XXX     /|        ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/        ")
+    print("     \_         _/          ")
+    print("       \_______/            ")
+
+def imprime_mensagem_vencedor():
+    print('Parabéns, você ganhou!')
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()

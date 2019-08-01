a2fe7f7b81c2ebb11c715123b7bb4c114c7fbc0c
thaisandre <thaisandre.sc@gmail.com>
2019-08-01 04:36:21 -0300

    exercicios cap02

---
+++ cap02/programa.py
@@ -0,0 +1,12 @@
+# código original
+print('Minha primeira aplicação Python')
+
+# ex1
+print('imprimindo uma mensagem diferente')
+
+# ex2
+print('Minha primeira aplicação Python')
+print('imprimindo uma mensagem diferente')
+
+# ex 3
+print('Minha primeira aplicação Python\nimprimindo uma mensagem diferente')
